38437f7af01cd628c38a562b672d2fa89312113e
mykola2312 <nikolay_tihonov@i.ua>
2024-01-04 21:09:12 +0200

    clean isr and msp files, they're going to be edited by hand, not CubeMX. they're part of our project now

--- src/hal_isr.h
+++ src/hal_isr.h
@@ -1,27 +1,5 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    stm32f4xx_it.h
-  * @brief   This file contains the headers of the interrupt handlers.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
- ******************************************************************************
-  */
-/* USER CODE END Header */
 #ifndef __STM32F4xx_IT_H
 #define __STM32F4xx_IT_H
-
-#ifdef __cplusplus
- extern "C" {
-#endif
 
 void NMI_Handler(void);
 void HardFault_Handler(void);
@@ -33,8 +11,4 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif /* __STM32F4xx_IT_H */
